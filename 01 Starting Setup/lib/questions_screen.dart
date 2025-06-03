1724ab62cd5e32747a31903c0052658ae55146f8
zihwanpack home <jihwan8807@naver.com>
2025-06-04 05:38:52 +0900

    init debug

--- 01 Starting Setup/lib/questions_screen.dart
+++ 01 Starting Setup/lib/questions_screen.dart
@@ -1,27 +1,32 @@
-import 'package:ads_basics/answer_button.dart';
 import 'package:flutter/material.dart';
-import 'package:ads_basics/data/questions.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
+
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key, required this.onSelectAnswer});
+  const QuestionsScreen({
+    super.key,
+    required this.onSelectAnswer,
+  });
 
   final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
-    return _QuestionScreenState();
+    return _QuestionsScreenState();
   }
 }
 
-class _QuestionScreenState extends State<QuestionsScreen> {
+class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answeredQuestion(String selectedAnswers) {
-    // widget 클래스는 state 확장할 때 쓰임
-    widget.onSelectAnswer(selectedAnswers);
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    // currentQuestionIndex += 1;
     setState(() {
-      currentQuestionIndex++;
+      currentQuestionIndex++; // increments the value by 1
     });
   }
 
@@ -30,7 +35,6 @@ class _QuestionScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
-      // 가능한 최대한 넓이
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
@@ -40,22 +44,22 @@ class _QuestionScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: GoogleFonts.notoSans(
-                color: const Color.fromARGB(255, 154, 187, 236),
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 201, 153, 251),
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
-            ...currentQuestion.getShuffledAnswer().map((answer) {
+            ...currentQuestion.shuffledAnswers.map((answer) {
               return AnswerButton(
                 answerText: answer,
                 onTap: () {
-                  answeredQuestion(answer);
+                  answerQuestion(answer);
                 },
               );
-            }),
+            })
           ],
         ),
       ),
